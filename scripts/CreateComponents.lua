98678e6a3a4fa9ecddc0c3c184d58a1e6ba474b1
Ruben Lopez <rlopezg@edu.tecnocampus.cat>
2022-10-28 16:47:32 +0200

    Comments
    Added comments on components

--- scripts/CreateComponents.lua
+++ scripts/CreateComponents.lua
@@ -7,3 +7,21 @@ Color = Component.create("color", { "r", "g", "b", "a" }, { r = 255, g = 255, b 
 Jump = Component.create("jump", { "jumpKey", "force", "rayActive", "timer", "maxTimer" }, { jumpKey = "w", force = -8, rayActive = true, timer = 0, maxTimer = 0.3 })
 MovementKeys = Component.create("movementKeys", { "left", "right" }, { left = "a", right = "d" })
 Direction = Component.create("direction", { "left", "right" }, { left = false, right = true })
+
+
+--[[
+
+-- GUÍA --
+
+Transform -> Almacena lo básico de lo básico, la posición, rotación, escala, altura y anchura y su nombre.
+Collider -> La que gestiona los colliders, tiene isTrigger por si queremos hacer que lo atraviess igualmente (perfecto para eventos, sin implementar todavía)
+                    después isColliding para el suelo, y isTouchingWall para las parades, todos en true o false
+Velocity -> Almacena la velocidad actual del jugador tanto en x o y
+Acceleration -> Se encarga de guardar la aceleración tanto de x o y individualmente de la velocidad, se podría modificar para hacer los saltos más crunchy
+Color -> Guarda las 3 variables r g b y la alpha en bytes (0 - 255)
+Jump -> Contiene las variables jumpKey (tecla para saltar), force (la fuerza del salto), rayActive (true o false) y desactiva o activa los raycasts del suelo, y timer y maxtimer (explicadas en sistemas)
+MovementKeys -> Contiene las teclas que se usarán para moverte
+Direction -> Almacena la dirección del jugador, está atada con movement keys-
+
+
+]]
