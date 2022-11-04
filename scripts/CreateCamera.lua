f4bec6dd435eced120e25a700519cca90bce5b54
Alexandro Vera <Alexandroverapons@gmail.com>
2022-11-04 13:24:39 +0100

    yuopui

--- scripts/CreateCamera.lua
+++ scripts/CreateCamera.lua
@@ -7,17 +7,14 @@
 -- X position (is created when you place the camera in tiled)
 -- Y position (is created when you place the camera in tiled)
 -- changeThreshold (set the *percentage* of the camera you need to exit to go to the next camera)
+    -- if you dont set a threshold then alternativeThreshold will be the default value
 
 --------------------------- Extra info about camera (para los gigaHead programadores)---------------------------------
 
 
-
-
-
--- change this to change the zoom of the camera
 local alternativeThreshold = 75 -- change this to change all non modified threshold values
 
-local cameraZoom
+local cameraZoom -- change this to change the zoom of the camera
 
 local currentPosX, currentPosY -- current position of the camera
 
@@ -33,11 +30,11 @@ end
 
 for j, position in pairs(cameraStates) do
     print(cameraStates[j].x)
-end    
+end
 
 Cam:lookAt(cameraStates[1].x, cameraStates[1].y)
 
-print (Cam:position())
+print(Cam:position())
 
 function CameraSystem:update(dt)
 
@@ -46,6 +43,7 @@ end
 function love.keypressed(key)
     nextSection()
 end
+
 function getCameraPosition()
     return currentPosX, currentPosY
 end
@@ -56,18 +54,23 @@ end
 
 -- function to change to next section
 function nextSection()
-    id = id + 1
-    currentPosX = cameraStates[id].x
-    currentPosY = cameraStates[id].y
+
+    if cameraStates[id + 1] then -- checks if the next level is null or not
+        id = id + 1
+        currentPosX = cameraStates[id].x
+        currentPosY = cameraStates[id].y
+    end
 
     Cam:lookAt(currentPosX, currentPosY)
 end
 
 -- function to change to the previous section
 function prevSection()
-    id = id - 1
-    currentPosX = cameraStates[id].x
-    currentPosY = cameraStates[id].y
+    if cameraStates[id - 1] then
+        id = id - 1
+        currentPosX = cameraStates[id].x
+        currentPosY = cameraStates[id].y
+    end
 
     Cam:lookAt(currentPosX, currentPosY)
 end
