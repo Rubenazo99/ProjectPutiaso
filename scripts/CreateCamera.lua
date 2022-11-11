752a4ad5cd2bf40375bcba797cb8a2cac37d7f5a
TeoSitja <tsitja@edu.tecnocampus.cat>
2022-11-11 13:25:18 +0100

    tuto a medio hacer

--- scripts/CreateCamera.lua
+++ scripts/CreateCamera.lua
@@ -92,9 +92,9 @@ end
 function love.keypressed(key)
     -- Enable for debug purposes
 
-    -- if key == 'i' then
-    --     nextSection()
-    -- end
+    if key == 'i' then
+        nextSection()
+    end
 
     -- if key == 'c' then
     --     for i, v in pairs(playerList) do
