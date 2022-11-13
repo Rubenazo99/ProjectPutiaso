dc0922bc0049672c74dd70814d4a12a210db6776
Eduard Sales <EvilHat2@gmail.com>
2022-11-13 23:04:49 +0100

    un push para el color de menu

--- scripts/CreateMenuEngine.lua
+++ scripts/CreateMenuEngine.lua
@@ -15,7 +15,7 @@ local ExitButton = Entity()
 ExitButton:initialize()
 
 ExitButton:add(Transform(-700, h/2, 0, 0, 1, w-200, h - 900, "exitButton")) -- "x", "y", "rotation", "scaleX", "scaleY", "width", "height", "name"
-ExitButton:add(Color(255, 255, 255))
+ExitButton:add(Color(0, 0, 233,1))
 
 MenuEngine = Engine()
 
